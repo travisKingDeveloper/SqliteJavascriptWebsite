b46a1bf9336cd47a1bc4efdde71e107fd9595f3d
Travis King <travis.king@beeline.com>
2017-02-15 18:03:27 -0500

    Creating new connection with DB every hit from api

--- api/src/routes/employee/employee.js
+++ api/src/routes/employee/employee.js
@@ -4,14 +4,16 @@
 let sqlite3 = require('sqlite3')
 let config = require('../../config')
 
-let db = new sqlite3.Database('../../db/employees.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.log(`Error opening database employees.db ${err}`)
-    }
-});
+let initDB = (error)=> {
+    return new sqlite3.Database('../../db/employees.db', sqlite3.OPEN_READWRITE, error)
+}
 
 let insertEmployee = (employee) => {
     return new Promise((resolve, reject) => {
+        initDB((err) => {
+            return reject(err)
+        })
+
         let sql =
             `INSERT INTO 
                 Employee (EmployeeID, Name, StartDate, EndDate, Salary, IsActive) 
@@ -19,8 +21,8 @@ let insertEmployee = (employee) => {
                 ('${employee.employeeID}', '${employee.name}', '${employee.startDate}',
                     '${employee.endDate ? employee.endDate : 'NULL'}', ${employee.salary}, ${employee.isActive ? 1 : 0});`
 
-        db.run(sql , undefined, (err) => {
-            if(err){
+        db.run(sql, undefined, (err) => {
+            if (err) {
                 reject(err)
             } else {
                 resolve()
