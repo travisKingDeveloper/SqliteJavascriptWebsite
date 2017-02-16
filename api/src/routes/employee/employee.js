8b9d886554567b437246c77ca1f1f4522ea478f5
KingTravis <travis_king4@yahoo.com>
2017-02-16 07:05:57 -0500

    Finished All Routes for now (Put, Delete, Get All, Get One, Post)

--- api/src/routes/employee/employee.js
+++ api/src/routes/employee/employee.js
@@ -35,7 +35,7 @@ let insertEmployee = (employee) => {
 let selectAllEmployees = () => {
     return new Promise((resolve, reject) => {
         let sql =
-            `SELECT * FROM Employee`
+            `SELECT * FROM Employee;`
 
         let results = []
 
@@ -63,7 +63,7 @@ let selectAllEmployees = () => {
         }
 
         let db = initDB((err) => {
-            if(err){
+            if (err) {
                 reject(err)
             }
         })
@@ -74,11 +74,93 @@ let selectAllEmployees = () => {
 
 let selectOneEmployee = (employeeID) => {
     return new Promise((resolve, reject) => {
-        let sql = `SELECT * FROM Employee WHERE EmployeeID = ${}`
+        let sql = `SELECT * FROM Employee WHERE EmployeeID = '${employeeID}';`
+
+        let rowHandler = (err, row) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve({
+                    name: row.Name,
+                    id: row.EmployeeID,
+                    startDate: row.StartDate,
+                    endDate: row.EndDate,
+                    salary: row.Salary,
+                    isActive: row.IsActive
+                })
+            }
+        }
+
+        let db = initDB((err) => {
+            if (err) {
+                reject(err)
+            }
+        })
+
+        db.get(sql, undefined, rowHandler)
+    })
+}
+
+let updateEmployee = (employee) => {
+    return new Promise((resolve, reject) => {
+        let employeeUpdateMapping = (employee) => {
+            let update = ''
+
+            update += `Name = '${employee.name}', `
+            update += `StartDate = ${employee.startDate}, `
+            update += `EndDate = ${employee.endDate ? employee.endDate : 'NULL'}, `
+            update += `Salary = ${employee.salary}, `
+            update += `IsActive = ${employee.isActive ? 1 : 0}`
+
+            return update
+        }
+
+        let sql =
+            `UPDATE Employee 
+             SET 
+                ${employeeUpdateMapping(employee)}
+             WHERE EmployeeID = '${employee.employeeID ? employee.employeeID : ''}'`
+
+        let db = initDB((err) => {
+            if(err){
+                reject(err)
+            }
+        })
+
+        db.run(sql, undefined, (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
+    })
+}
+
+let deleteEmployee = (employeeID) => {
+    return new Promise((resolve, reject) => {
+        let sql = `DELETE FROM Employee WHERE EmployeeID ='${employeeID}';`
+
+        let db = initDB((err) => {
+            if (err) {
+                reject(err)
+            }
+        })
+
+        db.run(sql, undefined, (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
     })
 }
 
 module.exports = {
     insertEmployee: insertEmployee,
-    selectAllEmployees: selectAllEmployees
+    selectAllEmployees: selectAllEmployees,
+    selectOneEmployee: selectOneEmployee,
+    updateEmployee: updateEmployee,
+    deleteEmployee: deleteEmployee
 }
--- api/src/routes/employee/index.js
+++ api/src/routes/employee/index.js
@@ -5,7 +5,7 @@
 let Router = require('restify-router').Router
 let uuid = require('node-uuid');
 let employeeInstance = new Router()
-let {insertEmployee, selectAllEmployees} = require('./employee')
+let {insertEmployee, selectAllEmployees, selectOneEmployee, updateEmployee, deleteEmployee} = require('./employee')
 
 employeeInstance.post('/', (req, res, next) => {
     let employee = {
@@ -38,6 +38,54 @@ employeeInstance.get('/', (req, res, next) => {
         })
         .catch((err) => {
             res.send(JSON.stringify(err))
+            next()
+        })
+})
+
+employeeInstance.get('/:employeeID', (req, res, next) => {
+    let employeeID = req.params.employeeID
+
+    selectOneEmployee(employeeID)
+        .then((employee) => {
+            res.send(employee)
+            next()
+        })
+        .catch((err) => {
+            res.send(err)
+            next()
+        })
+})
+
+employeeInstance.put('/:employeeID', (req, res, next) => {
+    let employee = {
+        name: req.body.name,
+        startDate: req.body.startDate,
+        salary: req.body.salary,
+        isActive: req.body.isActive,
+        employeeID: req.params.employeeID
+    }
+
+    updateEmployee(employee)
+        .then(() => {
+            res.send(JSON.stringify({success: true}))
+            next()
+        })
+        .catch((err) => {
+            res.send(err)
+            next()
+        })
+})
+
+employeeInstance.del('/:employeeID', (req, res, next) => {
+    let employeeID = req.params.employeeID
+
+    deleteEmployee(employeeID)
+        .then(() => {
+            res.send(JSON.stringify({success: true}))
+            next()
+        })
+        .catch((err) => {
+            res.send(err)
             next()
         })
 })
